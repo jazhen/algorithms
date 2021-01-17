df5808ee276af118c3bbdfeaf1ffc24eac65219a
Jason Zhen <jasonzhen.mail@gmail.com>
2021-01-17 01:52:05 -0800

    12-5

---
+++ grokking/modified_binary_search/12-5.js
@@ -0,0 +1,46 @@
+// Given an array of numbers sorted in ascending order, find the element
+// in the array that has the minimum difference with the given ‘key’.
+
+// Example 1:
+
+// Input: [4, 6, 10], key = 7
+// Output: 6
+// Explanation: The difference between the key '7' and '6' is minimum than
+//  any other number in the array
+
+// Example 2:
+
+// Input: [4, 6, 10], key = 4
+// Output: 4
+
+// Example 3:
+
+// Input: [1, 3, 8, 10, 15], key = 12
+// Output: 10
+
+// Example 4:
+
+// Input: [4, 6, 10], key = 17
+// Output: 10
+
+function searchMinDiffElement(arr, key) {
+  let start = 0;
+  let end = arr.length;
+
+  while (start < end) {
+    let mid = Math.floor(start + (end - start) / 2);
+
+    if (arr[mid] > key) {
+      end = mid;
+    } else {
+      start = mid + 1;
+    }
+  }
+
+  return arr[start - 1];
+}
+
+console.log(searchMinDiffElement([4, 6, 10], 7)); // 6
+console.log(searchMinDiffElement([4, 6, 10], 4)); // 4
+console.log(searchMinDiffElement([4, 6, 10], 12)); // 10
+console.log(searchMinDiffElement([4, 6, 10], 17)); // 10
