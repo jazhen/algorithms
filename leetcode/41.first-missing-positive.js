fba43c64093eddb800f8d47cb00e0ecec4d7c1b4
Jason Zhen <jasonzhen.mail@gmail.com>
2021-01-23 22:20:05 -0800

    https://leetcode.com/problems/first-missing-positive/

---
+++ leetcode/41.first-missing-positive.js
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/first-missing-positive/
+
+/*
+ * @lc app=leetcode id=41 lang=javascript
+ *
+ * [41] First Missing Positive
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+  let i = 0;
+
+  while (i < nums.length) {
+    let j = nums[i] - 1;
+
+    if (nums[i] > 0 && nums[i] < nums.length && nums[i] !== nums[j]) {
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+    } else {
+      i += 1;
+    }
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== i + 1) {
+      return i + 1;
+    }
+  }
+
+  return nums.length + 1;
+};
+// @lc code=end
+
