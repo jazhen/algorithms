d82c50f0c22e7ae4aab21cfbe0b5bc512dd6d02d
Jason Zhen <jasonzhen.mail@gmail.com>
2021-01-24 02:00:49 -0800

    Add descriptions for methods

--- data-structures/heap.js
+++ data-structures/heap.js
@@ -1,63 +1,105 @@
-/**
- * Defaults to min heap
- * @usage:
- * let heap = new Heap();
- *
- * min heap (default): comparator = (a, b) => a - b < 0
- * max heap          : comparator = (a, b) => b - a > 0
- */
-
 class Heap {
+  /**
+   * Create a Heap. Defaults to a min heap.
+   * @example
+   * const minHeap1 = new Heap([75, 12, 3, 11]);
+   * @example
+   * const maxHeap = new Heap([], (a, b) => a > b);
+   * @example
+   * const frequencies = [{num: 5, freq: 8}, {num: 30, freq: 1}, {num: 2, freq: 3}];
+   * const minHeap2 = new Heap(frequencies, (a, b) => a && b && a.val < b.val);
+   * @param {number[]} tree - The initial elements of the heap.
+   * @param {function} comparator - The function that determines the heap structure.
+   */
   constructor(tree = [], comparator = (a, b) => a < b) {
     this.tree = tree;
     this.comparator = comparator;
     this.buildHeap();
   }
 
-  // amortised O(n)
+  /**
+   * Heapify the given tree. Only runs on Heap initialization.
+   * Time - O(n) (amortized)
+   */
   buildHeap() {
     for (let i = Math.floor(this.size() / 2); i >= 0; i--) {
       this.siftDown(i);
     }
   }
 
-  // O(1)
+  /**
+   * Get the index of the given index's parent.
+   * Time - O(1)
+   * @param {number} index
+   * @return {number}
+   */
   getParentIndex(index) {
     return Math.floor((index - 1) / 2);
   }
 
-  // O(1)
+  /**
+   * Get the index of the given index's left child.
+   * Time - O(1)
+   * @param {number} index
+   * @return {number}
+   */
   getLeftChildIndex(index) {
     return index * 2 + 1;
   }
 
-  // O(1)
+  /**
+   * Get the index of the given index's right child.
+   * Time - O(1)
+   * @param {number} index
+   * @return {number}
+   */
   getRightChildIndex(index) {
     return index * 2 + 2;
   }
 
-  // O(1)
+  /**
+   * Get the number of nodes in the heap.
+   * Time - O(1)
+   * @return {number}
+   */
   size() {
     return this.tree.length;
   }
 
-  // O(1)
+  /**
+   * Swap the two nodes at the given indices.
+   * Time - O(1)
+   * @param {number} index1
+   * @param {number} index2
+   */
   swap(index1, index2) {
     [this.tree[index1], this.tree[index2]] = [this.tree[index2], this.tree[index1]];
   }
 
-  // O(1)
+  /**
+   * Get the root node.
+   * Time - O(1)
+   * @return {number}
+   */
   peek() {
     return this.size() ? this.tree[0] : null;
   }
 
-  // O(log n)
+  /**
+   * Insert a new node with the given value.
+   * Time - O(log n)
+   * @param {number} val
+   */
   insert(val) {
     this.tree.push(val);
     this.siftUp(this.size() - 1);
   }
 
-  // O(log n)
+  /**
+   * Remove the root node.
+   * Time - O(log n)
+   * @return {number}
+   */
   extract() {
     if (!this.size()) return null;
 
@@ -67,7 +109,11 @@ class Heap {
     return root;
   }
 
-  // O(log n)
+  /**
+   * Maintain heap structure from the given index to the root.
+   * Time - O(log n)
+   * @param {number} index
+   */
   siftUp(index) {
     if (index === 0) return;
 
@@ -78,7 +124,13 @@ class Heap {
     }
   }
 
-  // O(1)
+  /**
+   * Get the index of the child that has the highest priority when passed
+   * through the comparator function.
+   * Time - O(1)
+   * @param {number} index
+   * @return {number}
+   */
   maxPriorityChildIndex(index) {
     const rightChildIndex = this.getRightChildIndex(index);
     const leftChildIndex = this.getLeftChildIndex(index);
@@ -87,7 +139,11 @@ class Heap {
       rightChildIndex : leftChildIndex;
   }
 
-  // O(log n)
+  /**
+   * Maintain heap structure from the given index to the last node.
+   * Time - O(log n)
+   * @param {number} index
+   */
   siftDown(index) {
     const childIndex = this.maxPriorityChildIndex(index);
     if (this.comparator(this.tree[childIndex], this.tree[index])) {
