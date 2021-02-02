e0b9fefdee7601b562ac6e361409bb0e5f265d4a
Jason Zhen <jasonzhen.mail@gmail.com>
2021-02-02 03:37:59 -0800

    https://leetcode.com/problems/longest-increasing-subsequence/

---
+++ leetcode/300.longest-increasing-subsequence.js
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/longest-increasing-subsequence/
+
+/*
+ * @lc app=leetcode id=300 lang=javascript
+ *
+ * [300] Longest Increasing Subsequence
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+  const dp = new Array(nums.length + 1).fill(1);
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+
+  return Math.max(...dp);
+};
+// @lc code=end
