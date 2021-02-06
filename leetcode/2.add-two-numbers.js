d871666b825ea45f2cc2c7055b66ea3b34bd990f
Jason Zhen <jasonzhen.mail@gmail.com>
2021-02-06 14:40:35 -0800

    https://leetcode.com/problems/add-two-numbers/

--- leetcode/2.add-two-numbers.js
+++ leetcode/2.add-two-numbers.js
@@ -22,18 +22,17 @@
 var addTwoNumbers = function(l1, l2) {
   let dummyHead = new ListNode();
   let curr = dummyHead;
-  let sum = 0;
   let carry = 0;
 
   while (l1 || l2) {
     let a = l1 ? l1.val : 0;
     let b = l2 ? l2.val : 0;
 
-    sum = a + b + carry;
+    let sum = a + b + carry;
     carry = Math.floor(sum / 10);
     curr.next = new ListNode(sum % 10);
-    curr = curr.next;
 
+    curr = curr.next;
     if (l1) l1 = l1.next;
     if (l2) l2 = l2.next;
   }
