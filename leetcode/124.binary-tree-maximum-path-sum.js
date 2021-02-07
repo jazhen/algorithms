770a6071d092f93c049216e4aca66ddc73845ce9
Jason Zhen <jasonzhen.mail@gmail.com>
2021-02-07 04:17:47 -0800

    https://leetcode.com/problems/binary-tree-maximum-path-sum/

---
+++ leetcode/124.binary-tree-maximum-path-sum.js
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/binary-tree-maximum-path-sum/
+
+/*
+ * @lc app=leetcode id=124 lang=javascript
+ *
+ * [124] Binary Tree Maximum Path Sum
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxPathSum = function(root) {
+  let max = -Infinity;
+
+  function dfs(root, sum) {
+    if (!root) return 0;
+
+    let left = Math.max(dfs(root.left), 0);
+    let right = Math.max(dfs(root.right), 0);
+
+    max = Math.max(max, left + right + root.val);
+
+    return Math.max(left, right) + root.val;
+  }
+
+  dfs(root);
+  return max;
+};
+
+// time: O(n)
+// space: O(n)
+// @lc code=end
+
