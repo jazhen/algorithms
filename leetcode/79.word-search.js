902998a1f989ff20b3668ec9e02d55bb6b2e0d87
Jason Zhen <jasonzhen.mail@gmail.com>
2021-02-18 02:23:21 -0800

    https://leetcode.com/problems/word-search/

--- leetcode/79.word-search.js
+++ leetcode/79.word-search.js
@@ -11,28 +11,28 @@ function findWord(board, word, m, n, i, j, pos) {
   // out of bounds
   if (i < 0 || i >= m || j < 0 || j >= n) return false;
 
-  // letter is already used
-  if (board[i][j] === '') return false;
-
-  // not the correct letter needed; return early
+  // not the correct letter needed
   if (board[i][j] !== word[pos]) return false;
 
   // found the word
   if (pos === word.length - 1) return true;
 
-  // mark the current board position as used
-  const temp = board[i][j];
-  board[i][j] = '';
-
   // recursively check 4 directional
-  for (const dir of [[1, 0], [-1, 0], [0, 1], [0, -1]]) {
-    if (findWord(board, word, m, n, i + dir[0], j + dir[1], pos + 1)) {
+  for (const [x, y] of [[1, 0], [-1, 0], [0, 1], [0, -1]]) {
+    // mark the current board position as used
+    const temp = board[i][j];
+    board[i][j] = '';
+
+    // go deeper
+    if (findWord(board, word, m, n, i + x, j + y, pos + 1)) {
       return true;
     }
+
+    // backtrack
+    board[i][j] = temp;
   }
 
-  // backtrack
-  board[i][j] = temp;
+  return false;
 }
 /**
  * @param {character[][]} board
@@ -42,11 +42,10 @@ function findWord(board, word, m, n, i, j, pos) {
 var exist = function(board, word) {
   const m = board.length;
   const n = board[0].length;
-  let pos = 0;
 
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
-      if (board[i][j] === word[0] && findWord(board, word, m, n, i, j, pos)) {
+      if (findWord(board, word, m, n, i, j, 0)) {
         return true;
       }
     }
