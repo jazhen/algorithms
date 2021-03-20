0108fd2b976fe303dc515183c521c560359beb79
Jason Zhen <jasonzhen.mail@gmail.com>
2021-03-20 01:57:57 -0700

    https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/

--- leetcode/34.find-first-and-last-position-of-element-in-sorted-array.js
+++ leetcode/34.find-first-and-last-position-of-element-in-sorted-array.js
@@ -12,49 +12,42 @@
  * @param {number} target
  * @return {number[]}
  */
-var searchRange = function(nums, target) {
-  const result = [-1, -1];
+ var searchRange = function(nums, target) {
+  const ans = [-1, -1];
 
-  // try to find the first index of target
-  let start = 0;
-  let end = nums.length - 1;
+  ans[0] = binarySearch(nums, target, false);
 
-  while (start <= end) {
-    const mid = Math.floor(start + (end - start) / 2);
-
-    if (nums[mid] > target) {
-      end = mid - 1;
-    } else if (nums[mid] < target) {
-      start = mid + 1;
-    } else {
-      // narrow down to the first appearance of target
-      result[0] = mid;
-      end = mid - 1;
-    }
+  if (ans[0] !== -1) {
+    ans[1] = binarySearch(nums, target, true);
   }
 
-  // if target not found in prev step return [-1, -1]
-  if (result[0] === -1) return result;
-
-  // same process for last index of target
-  start = 0;
-  end = nums.length - 1;
-
-  while (start <= end) {
-    const mid = Math.floor(start + (end - start) / 2);
-
-    if (nums[mid] > target) {
-      end = mid - 1;
-    } else if (nums[mid] < target) {
-      start = mid + 1;
-    } else {
-      // narrow down to the second appearance of target
-      result[1] = mid;
-      start = mid + 1;
-    }
-  }
-
-  return result;
+  return ans;
 };
+
+function binarySearch(nums, target, hasTarget) {
+  let left = 0;
+  let right = nums.length - 1;
+  let targetIndex = -1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      targetIndex = mid;
+
+      if (hasTarget) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+
+  return targetIndex;
+}
 // @lc code=end
 
