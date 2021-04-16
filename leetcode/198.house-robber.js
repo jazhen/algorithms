faa1996b8e7663fb03fbffab99c9f961e6e3c0c7
Jason Zhen <jasonzhen.mail@gmail.com>
2021-04-16 03:01:20 -0700

    https://leetcode.com/problems/house-robber/

--- leetcode/198.house-robber.js
+++ leetcode/198.house-robber.js
@@ -12,22 +12,18 @@
  * @return {number}
  *
  * time: O(nums.length)
- * space: O(nums.length)
+ * space: O(1)
  */
 var rob = function(nums) {
-  if (nums.length === 1) {
-    return nums[0];
-  }
+  if (nums.length === 1) return nums[0];
 
-  const dp = new Array(nums.length).fill(0);
-  dp[0] = nums[0];
-  dp[1] = Math.max(nums[0], nums[1]);
+  nums[1] = Math.max(nums[0], nums[1]);
 
   for (let i = 2; i < nums.length; i++) {
-    dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+    nums[i] = Math.max(nums[i - 1], nums[i - 2] + nums[i]);
   }
 
-  return dp[dp.length - 1];
+  return nums[nums.length - 1];
 };
 // @lc code=end
 
