5b0408ce25e0caff3d51e69b609ac3784846fc7e
Jason Zhen <jasonzhen.mail@gmail.com>
2021-05-27 23:59:11 -0700

    https://leetcode.com/problems/backspace-string-compare/

--- leetcode/844.backspace-string-compare.js
+++ leetcode/844.backspace-string-compare.js
@@ -13,6 +13,55 @@
  * @return {boolean}
  *
  * time: O(s.length + t.length)
+ * space: O(1)
+ */
+ var backspaceCompareTwoPointer = function(s, t) {
+  let sIndex = s.length - 1;
+  let tIndex = t.length - 1;
+
+  let sBackspaces = 0;
+  let tBackspaces = 0;
+
+  while (sIndex >= 0 || tIndex >= 0) {
+    if (s[sIndex] === '#') {
+      sBackspaces++;
+      sIndex--;
+      continue;
+    }
+
+    if (sBackspaces > 0) {
+      sBackspaces--;
+      sIndex--;
+      continue;
+    }
+
+    if (t[tIndex] === '#') {
+      tBackspaces++;
+      tIndex--;
+      continue;
+    }
+
+    if (tBackspaces > 0) {
+      tBackspaces--;
+      tIndex--;
+      continue;
+    }
+
+    if (s[sIndex] !== t[tIndex]) return false;
+
+    sIndex--;
+    tIndex--;
+  }
+
+  return true;
+};
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ *
+ * time: O(s.length + t.length)
  * space: O(s.length + t.length)
  */
  var backspaceCompareStack = function(s, t) {
