6ac145d51a9fe7d7789d6e7b40596f10476688ce
Jason Zhen <jasonzhen.mail@gmail.com>
2021-05-30 23:58:40 -0700

    update union find implementation

--- templates/data-structures/UnionFind.js
+++ templates/data-structures/UnionFind.js
@@ -2,9 +2,9 @@ class UnionFind {
   /**
    * Create a data structure that stores a collection of disjoint sets.
    *
-   * @param {number} size - The initial number of disjoint sets.
+   * @param {number} size - The initial number of disjoint sets to make.
    */
-  constructor(size) {
+  constructor(size = 0) {
     this.numDisjointSets = size;
     this.parent = new Array(size);
     this.rank = new Array(size);
@@ -17,9 +17,6 @@ class UnionFind {
 
   /**
    * Add a new disjoint set containing only element x.
-   *
-   * @param {number} index
-   * @return {number}
    */
   makeSet(x) {
     this.parent[x] = x;
@@ -29,10 +26,6 @@ class UnionFind {
 
   /**
    * Return whether the given sets are connected.
-   *
-   * @param {number} x
-   * @param {number} y
-   * @return {boolean}
    */
   isConnected(x, y) {
     return this.find(x) === this.find(y);
@@ -42,34 +35,6 @@ class UnionFind {
    * Get the root of the element x using path compression.
    * The idea of path compression is to make the found root the parent of x
    * so that we don’t have to traverse all intermediate nodes again.
-   *
-   * Time -
-   *   Naive: O(n) worst case
-   *   Path compression: O(log(n)) worst case
-   *
-   * @example
-   *
-   *             9
-   *         /   |   \
-   *        4    5    6
-   *      /   \     /   \
-   *     0     3   7     8
-   *         /   \
-   *        1     2
-   *
-   *
-   *           find(3)
-   *
-   *
-   *              9
-   *        /  |    |    \
-   *       /   |    |     \
-   *      4    5    6      3
-   *    /          / \    / \
-   *   0          7   8  1   2
-   *
-   * @param {number} x
-   * @return {number}
    */
   find(x) {
     if (this.parent[x] !== x) {
@@ -84,14 +49,6 @@ class UnionFind {
    * The higher rank tree becomes the parent of the tree with a smaller rank.
    * If the ranks are the same, we arbitrarily choose rootX to be the parent
    * and increment its rank.
-   *
-   * Time -
-   *   Naive: O(n) worst case, O(log(n)) average case
-   *   Union by rank: O(log(n)) worst case
-   *
-   * @param {number} x
-   * @param {number} y
-   * @return {boolean}
    */
   union(x, y) {
     const rootX = this.find(x);
