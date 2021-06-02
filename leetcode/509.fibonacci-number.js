0363f927e17d0143893bd79e84999c8c6a8e935c
Jason Zhen <jasonzhen.mail@gmail.com>
2021-06-01 23:54:21 -0700

    https://leetcode.com/problems/fibonacci-number/

--- leetcode/509.fibonacci-number.js
+++ leetcode/509.fibonacci-number.js
@@ -10,9 +10,42 @@
 /**
  * @param {number} n
  * @return {number}
+ *
+ * time: O(2^n)
+ * space: O(n)
  */
- var fib = function(n) {
-  const dp = new Array(n + 1).fill(0);
+ var fibRecursive = function(n) {
+  if (n <= 0) return 0;
+  if (n === 1) return 1;
+
+  return fib(n - 1) + fib(n - 2);
+};
+
+/**
+ * @param {number} n
+ * @return {number}
+ *
+ * time: O(n)
+ * space: O(n)
+ */
+var fibMemo = function(n, memo = new Map([[0, 0], [1, 1]])) {
+  if (memo.has(n)) return memo.get(n);
+
+  memo.set(n, fib(n - 1) + fib(n - 2));
+
+  return memo.get(n);
+};
+
+/**
+ * @param {number} n
+ * @return {number}
+ *
+ * time: O(n)
+ * space: O(n)
+ */
+ var fibDP = function(n) {
+  const dp = new Array(n + 1).fill(null);
+  dp[0] = 0;
   dp[1] = 1;
 
   for (let i = 2; i <= n; i++) {
@@ -20,5 +53,25 @@
   }
 
   return dp[n];
+};
+
+/**
+ * @param {number} n
+ * @return {number}
+ *
+ * time: O(n)
+ * space: O(1)
+ */
+ var fibIterative = function(n) {
+  if (n <= 1) return n;
+
+  let prev = 0;
+  let curr = 1;
+
+  for (let i = 2; i <= n; i++) {
+    [prev, curr] = [curr, prev + curr];
+  }
+
+  return curr;
 };
 // @lc code=end
