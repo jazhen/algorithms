9ab1c3d5d28f4bb84535ab8845b58ab9b11ee5e3
Jason Zhen <jasonzhen.mail@gmail.com>
2021-06-05 19:56:10 -0700

    https://leetcode.com/problems/reverse-integer/

--- leetcode/7.reverse-integer.js
+++ leetcode/7.reverse-integer.js
@@ -1,33 +1,41 @@
-// https://leetcode.com/problems/reverse-integer/
-
 /*
  * @lc app=leetcode id=7 lang=javascript
  *
  * [7] Reverse Integer
+ *
+ * https://leetcode.com/problems/reverse-integer/
  */
 
 // @lc code=start
 /**
  * @param {number} x
  * @return {number}
+ *
+ * time: O(log_10(x))
+ * space: O(1)
  */
-var reverse = function(x) {
-  let stringX = x.toString();
-  let reversedX;
-
-  if (stringX[0] === '-') {
-    reversedX = '-' + stringX.slice(1).split('').reverse().join('');
-  } else {
-    reversedX = stringX.split('').reverse().join('');
-  }
-
-  reversedX = parseInt(reversedX, 10);
-
-  if (reversedX < (-2) ** 31 || reversedX > 2 ** 31) {
-    return 0;
-  }
-
-  return reversedX;
+ var reverse = function(x) {
+  const isNegative = x < 0;
+  const reversed = isNegative ? -getReversed(Math.abs(x)) : getReversed(x);
+  return is32Bit(reversed) ? reversed : 0;
 };
+
+function getReversed(n) {
+  let reversed = 0;
+
+  while (n > 0) {
+    reversed = (10 * reversed) + (n % 10);
+    n = Math.floor(n / 10);
+  }
+
+  return reversed;
+}
+
+function is32Bit(n) {
+  const min = -Math.pow(2, 31);
+  const max = Math.pow(2, 31) -1;
+
+  return n >= min && n <= max;
+}
 // @lc code=end
 
