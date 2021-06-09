21f71c32266c85ce1d10b5973e8a57c01bd43bd7
Jason Zhen <jasonzhen.mail@gmail.com>
2021-06-08 20:13:18 -0700

    Update overlapping interval methods

--- templates/overlappingIntervals.js
+++ templates/overlappingIntervals.js
@@ -7,6 +7,7 @@ function isOverlapping(intervalA, intervalB) {
   const start = Math.max(intervalA[0], intervalB[0]);
   const end = Math.min(intervalA[1], intervalB[1]);
 
+  /** use > only if [1, 3] and [3, 5] would be considered overlapping */
   return end - start >= 0;
 }
 
@@ -15,9 +16,22 @@ function isOverlapping(intervalA, intervalB) {
  * @param {number[]} invervalB
  * @return {number[]}
  */
-function mergeIntervals(intervalA, intervalB) {
+function getMergedInterval(intervalA, intervalB) {
   const start = Math.min(intervalA[0], intervalB[0]);
   const end = Math.max(intervalA[1], intervalB[1]);
+
+  return [start, end];
+}
+
+
+/**
+ * @param {number[]} intervalA
+ * @param {number[]} invervalB
+ * @return {number[]}
+ */
+function getOverlappingInterval(intervalA, intervalB) {
+  const start = Math.max(intervalA[0], intervalB[0]);
+  const end = Math.min(intervalA[1], intervalB[1]);
 
   return [start, end];
 }
