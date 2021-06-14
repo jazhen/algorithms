51830d7c1b831f5e65fdea1668f8ec0b0fe358ef
Jason Zhen <jasonzhen.mail@gmail.com>
2021-06-13 21:35:42 -0700

    https://leetcode.com/problems/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/

--- leetcode/1465.maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.js
+++ leetcode/1465.maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.js
@@ -14,29 +14,26 @@
  * @param {number[]} verticalCuts
  * @return {number}
  *
- * time: O(nlogn + mlogm), n = horizontalCuts.length, m = verticalCuts.length
+ * time: n * log(n) + m * log(m), n = horizontalCuts.length, m = verticalCuts.length
  * space: O(1)
  */
-var maxArea = function(h, w, horizontalCuts, verticalCuts) {
-  horizontalCuts.push(0, h);
-  horizontalCuts.sort((a, b) => a - b);
+var maxArea = function (h, w, horizontalCuts, verticalCuts) {
+  const maxHeight = getMaxGap(horizontalCuts, h);
+  const maxWidth = getMaxGap(verticalCuts, w);
 
-  let maxHeight = 0;
-
-  for (let i = 1; i < horizontalCuts.length; i++) {
-    maxHeight = Math.max(maxHeight, horizontalCuts[i] - horizontalCuts[i - 1]);
-  }
-
-  verticalCuts.push(0, w);
-  verticalCuts.sort((a, b) => a - b);
-
-  let maxWidth = 0;
-
-  for (let i = 1; i < verticalCuts.length; i++) {
-    maxWidth = Math.max(maxWidth, verticalCuts[i] - verticalCuts[i - 1]);
-  }
-
-  return (maxWidth % 1e+7 * maxHeight % 1e+7);
+  return (BigInt(maxHeight) * BigInt(maxWidth)) % BigInt(10 ** 9 + 7);
 };
-// @lc code=end
 
+function getMaxGap(cuts, size) {
+  cuts.push(0, size);
+  cuts.sort((a, b) => a - b);
+
+  let maxGap = 0;
+
+  for (let i = 1; i < cuts.length; i++) {
+    maxGap = Math.max(maxGap, cuts[i] - cuts[i - 1]);
+  }
+
+  return maxGap;
+}
+// @lc code=end
