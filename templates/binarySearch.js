337fd2228e69ea7cdc3e903a34f67652bd2f59a8
Jason Zhen <jasonzhen.mail@gmail.com>
2021-06-28 23:09:35 -0700

    Add new variation of binary search

--- templates/binarySearch.js
+++ templates/binarySearch.js
@@ -1,11 +1,11 @@
+/** https://leetcode.com/discuss/general-discussion/786126/python-powerful-ultimate-binary-search-template-solved-many-problems **/
+
 /**
- * https://leetcode.com/discuss/general-discussion/786126/python-powerful-ultimate-binary-search-template-solved-many-problems
- *
  * @param {number[]} array
  * @param {number} target
  * @return {number}
  */
-function binarySearch(array, target) {
+function binarySearchA(array, target) {
   /**
    * Correctly initialize the boundary variables start and end
    * to specify search space. Only one rule: set up the boundary to
@@ -30,6 +30,41 @@ function binarySearch(array, target) {
 
   /**
    * After exiting the while loop, start is the minimal k​
+   * satisfying the condition function.
+   */
+  return nums[start] === target ? start : -1;
+}
+
+/**
+ * @param {number[]} array
+ * @param {number} target
+ * @return {number}
+ */
+function binarySearchB(array, target) {
+  /**
+   * Correctly initialize the boundary variables start and end
+   * to specify search space. Only one rule: set up the boundary to
+   * include all possible elements.
+   * @example - if the answer can be less than array[start] or greater than array[end]
+   * let start = -1;
+   * let end = array.length;
+   */
+  let start = 0;
+  let end = array.length - 1;
+
+  while (start < end) {
+    let mid = Math.ceil(start + (end - start) / 2);
+
+    // Design the condition function.
+    if (nums[mid] <= target) {
+      start = mid;
+    } else {
+      end = mid - 1;
+    }
+  }
+
+  /**
+   * After exiting the while loop, start is the maximal k​
    * satisfying the condition function.
    */
   return nums[start] === target ? start : -1;
